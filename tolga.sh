cf686e21f2fad1574e106ea0cbbbadbbc8148a93
kingtolga <eroktolga@gmail.com>
2023-08-20 11:31:42 +0800

    update Sun 20 Aug 2023 11:31:42 AWST

---
+++ tolga.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# idea's for dave
+
+config_files=(
+    /$HOME/Programs
+    /$HOME/Documents
+    /$HOME/shiva
+)
+
+git_dir="$HOME/shiva.git"  # Use the correct path to the Git repository
+work_tree="$HOME/shiva"    # Use the correct path to the working tree
+
+# Navigate to the working tree directory
+cd "$work_tree" || exit
+
+for file in "${config_files[@]}"; do
+    git add "$file"
+done
+
+git commit -m "update $(date)"
+git push origin main  # Replace 'master' with the appropriate branch name
