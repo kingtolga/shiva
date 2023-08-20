530efd52ffffcaad185e852d37a4aea212dfaa2f
kingtolga <eroktolga@gmail.com>
2023-08-20 12:14:37 +0800

    commit all deletions

--- tolga.sh
+++ tolga.sh
@@ -8,6 +8,16 @@ config_files=(
     "shiva"
 )
 
+# Check if the remote URL is set to SSH
+remote_url=$(git remote get-url origin)
+
+if [[ $remote_url == *"git@github.com"* ]]; then
+    echo "Remote URL is set to SSH. Proceeding with the script..."
+else
+    echo "Remote URL is not set to SSH. Please set up SSH key-based authentication for the remote repository."
+    exit 1
+fi
+
 git_dir="$HOME/shiva/shiva.git"
 work_tree="$HOME/shiva"
 
