a2ab7b8d2993c78bfcb5b14b744e879c85158e90
kingtolga <eroktolga@gmail.com>
2023-08-20 12:03:06 +0800

    update Sun 20 Aug 2023 12:03:06 AWST

--- tolga.sh
+++ tolga.sh
@@ -3,13 +3,12 @@
 # idea's for dave
 
 config_files=(
-    "$HOME/Programs/*"
-    "$HOME/Documents/*"
+
     "$HOME/shiva/*"
 )
 
-git_dir="$HOME/shiva.git"  # Use the correct path to the Git repository
-work_tree="$HOME/shiva"    # Use the correct path to the working tree
+git_dir="$HOME/shiva.git"
+work_tree="$HOME/shiva"
 
 # Navigate to the working tree directory
 cd "$work_tree" || exit
