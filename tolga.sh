35e32fb6d42e5537f46ce9906ec142cd7f745ff7
kingtolga <eroktolga@gmail.com>
2023-08-20 13:14:01 +0800

    Edited commit @ 01:14 PM

--- tolga.sh
+++ tolga.sh
@@ -17,7 +17,7 @@ else
     echo "If you haven't already, generate an SSH key pair:"
     echo "ssh-keygen -t ed25519 -C 'your email'"
     echo "Add your SSH key to the agent:"
-    echo "eval \$(ssh-agent -s)"
+    echo "eval $(ssh-agent -s)"
     echo "ssh-add ~/.ssh/id_ed25519"
     echo "Then, add your SSH public key to your GitHub account:"
     echo "cat ~/.ssh/id_ed25519.pub"
@@ -27,49 +27,32 @@ else
     exit 1
 fi
 
-# Fetch and prune remote branches to optimize repository
-git fetch --prune
-echo "Fetched remote changes and pruned branches"
-
 git_dir="$HOME/shiva/shiva.git"
 work_tree="$HOME/shiva"
 
 # Navigate to the working tree directory
 cd "$work_tree" || exit
 
-# Pull remote changes using rebase to avoid unnecessary merge commits
-git pull origin main --rebase
-echo "Pulled remote changes using rebase"
+# Pull remote changes using merge
+git pull origin main --no-rebase
+echo "Pulled remote changes using merge"
 
 # Add and commit local changes
 for path in "${config_files[@]}"; do
     git add "$path"
 done
 
-# Check if there are changes to commit
-if ! git diff-index --quiet HEAD; then
-    commit_time=$(date +"%I:%M %p")  # 12-hour format
-    git commit -m "Update files at $commit_time"
-    commit_action="Committed changes"
-else
-    commit_action="No changes to commit"
-fi
+commit_time=$(date +"%I:%M %p")  # 12-hour format
+git commit -m "update $(date) at $commit_time"
+echo "Committed local changes"
 
 # Handle file deletions
 git add --all
-if [[ $(git diff --staged --name-status) == *"D"* ]]; then
-    deletion_time=$(date +"%I:%M %p")  # Update deletion time
-    git commit -m "Delete files at $deletion_time"
-    deletion_action="Committed deletions"
-else
-    deletion_action="No deletions to commit"
-fi
+commit_time=$(date +"%I:%M %p")  # Update commit time
+git commit -m "Edited commit @ $commit_time"
+echo "Committed edits"
 
 # Push changes to remote
 push_time=$(date +"%I:%M %p")  # Update push time
 git push origin main
 echo "Pushed changes to remote repository at $push_time"
-
-# Display commit and deletion actions
-echo "$commit_action"
-echo "$deletion_action"
